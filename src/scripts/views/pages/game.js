5d73ae2051a19f1f7c6044800285415de7cdc57a
Artand <martinchandra99@gmail.com>
2022-11-26 22:25:04 +0700

    fix accesibility keyboard - memory game done

--- src/scripts/views/pages/game.js
+++ src/scripts/views/pages/game.js
@@ -74,6 +74,14 @@ const Game = {
         cardTwo = card;
         sameCardCheck();
       }));
+
+      cardItem.forEach((card) => card.addEventListener('keydown', (e) => {
+        const keyDown = e.key !== undefined ? e.key : e.keyCode;
+        if ((keyDown === 'Enter' || keyDown === 13) || (['Spacebar', ' '].indexOf(keyDown) >= 0 || keyDown === 32)) {
+          e.preventDefault();
+          card.click();
+        }
+      }));
     };
     cardEvent(cards);
 
