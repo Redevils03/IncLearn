6c2e149ffc49b40e775aaec1eaf48452235c003a
nova <novawirananda97@gmail.com>
2022-11-29 00:30:52 +0800

    memperbaiki responsive + halaman video

--- src/scripts/views/pages/video.js
+++ src/scripts/views/pages/video.js
@@ -1,8 +1,40 @@
 const Video = {
   async render() {
     return `
-        <h2>Halaman Video</h2>
-        `;
+      <div class="content" style="min-height: 100%">
+        <h2 style="margin: 50px 0; text-align: center">Halaman Video</h2>
+        <search-bar class="search-bar"></search-bar>
+        <article id="articles" class="articles">
+          <div class="article-item">
+            <div class="article-item__header">
+              <img class="article-item__header__poster" src="./images/Learning-pana.png" alt="test">
+            </div>
+            <div class="article-item__content">
+              <h3 class=""article__title">Tips Beradaptasi di Lingkungan Baru</h3>
+              <span>author</span>
+            </div>
+          </div>
+          <div class="article-item">
+            <div class="article-item__header">
+              <img class="article-item__header__poster" src="./images/Vowels-amico.png" alt="test">
+            </div>
+            <div class="article-item__content">
+              <h3 class=""article__title">Test</h3>
+              <span>author</span>
+            </div>
+          </div>
+          <div class="article-item">
+            <div class="article-item__header">
+              <img class="article-item__header__poster" src="./images/Webinar-pana.png" alt="test">
+            </div>
+            <div class="article-item__content">
+              <h3 class=""article__title">Test</h3>
+              <span>author</span>
+            </div>
+          </div>
+        </article>
+      </div>
+    `;
   },
 };
 
