92355bd236efedca20e48093e23334cbe85e9716
Dody Setiawan <103734478+dody-setiawan@users.noreply.github.com>
2022-12-08 23:48:34 +0700

    memperbarui templat detail artikel
    memperbarui tampilan detail artikel

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -11,10 +11,21 @@ const createArticleTemplate = (article) => `
   </div></a>
 `;
 
-const createArticleDetailTemplate = (article) => `
-  <img class="card-item__header__poster" src="${article.image}" alt="${article.title}">
-  <p>${article.body}</p>
-`;
+const createArticleDetailTemplate = (article) => {
+  let articleContentContainer = '';
+  article.body.forEach((paragraph) => {
+    articleContentContainer += `<p class="article-content">${paragraph}</p><br>`;
+  });
+
+  return `
+    <div class="card-item__article-detail">
+      <h2>${article.title}</h2>
+      <img class="card-item__header__poster" src="${article.image}" alt="${article.title}">
+      <p class="article-info">Taken from: <a href="${article.resource}">${article.from}</a> | ${article.updatedAt}</p>
+      <div class="article-content-container">${articleContentContainer}</div>
+    </div>
+  `;
+};
 
 const createVideoTemplate = (video) => `
   <div class="card-item">
